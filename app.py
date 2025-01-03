149866be043297ea6bba193a76b269237876e612
samudhan2008 <159536401+samudhan2008@users.noreply.github.com>
2025-01-03 16:25:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,3 +70,6 @@ def download_note(note_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
